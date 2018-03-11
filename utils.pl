4fa96155d46f015418f34c787a21857b313e668f
Martin Géraud <martin.geraud@epitech.eu>
2018-03-10 21:07:49 -0500

    final

--- utils.pl
+++ utils.pl
@@ -1,20 +1,19 @@
 % add_to_end/3 ajoute une valeur à la fin d'une liste (Params : 1er = valeur, 2e = liste, 3e = liste retournée)
-add_to_end(X,Y,Z) :- append(Y,[X],Z).
+add_to_end(Element,List,NewList) :- append(List,[Element],NewList).
 
-% end_of_list/2 vérifie que le dernier élément de L est égal à E (Params : L = liste, E = élément)
+% end_of_list/2 vérifie que le dernier élément de List est égal à Element (Params : 1er = liste, 2e = élément)
 end_of_list([], _).
-end_of_list(L, E):- last(L,E).
+end_of_list(List, Element):- last(List,Element).
 
-% sublist/2 retourne une sous-liste de L (Params : S = sous-liste, L = liste)
-prefix(P,L):-append(P,_,L).
-sublist(S,L):-prefix(S,L).
-sublist(S,[_|T]):-sublist(S,T).
+% sublist/2 retourne une sous-liste de List (Params : 1er = sous-liste, 2e = liste)
+sublist(SubList,List):- append(SubList,_,List).
+sublist(SubList,[_|TrimmedList]):- sublist(SubList,TrimmedList).
 
-% lenght/2 retourne la longueur de la liste L (Params : L = liste, N = longueur)
+% lenght/2 retourne la longueur de la liste List (Params : 1er = liste, 2e = longueur)
 lenght([],0).
-lenght([_|L],N):- lenght(L,N1),
-				  N is N1+1.
+lenght([_|List],Lenght):-	lenght(List,NewLenght),
+							Lenght is NewLenght+1.
 
-% get_element_from_index/3 retourne l'élément d'une liste à partir d'un index donné (Params : IDX = index, L = liste, E = élément)
-get_element_from_index(IDX, L, []):- lenght(L, N1), N1 < IDX.
-get_element_from_index(IDX, L, E):- nth1(IDX, L, E).
+% get_element_from_index/3 retourne l'élément d'une liste à partir d'une position donnée (Params : 1er = position, 2e = liste, 3e = élément)
+get_element_from_index(Position, List, []):- lenght(List, Lenght), Lenght < Position.
+get_element_from_index(Position, List, Element):- nth1(Position, List, Element).
